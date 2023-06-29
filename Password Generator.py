718832ff78f484c250c7c6ef0f55c25b5dab2491
GEckett <grant.eckett@gmail.com>
2023-06-29 21:42:23 +0100

    Completed

---
+++ Password Generator.py
@@ -0,0 +1,19 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+print("Welcome to the PyPassword Generator!")
+nr_letters =int(input("How Many Letters?\n"))
+nr_symbols =int(input("How Many Symbols?\n"))
+nr_numbers =int(input("How Many Numbers?\n"))
+password = ""
+for car in range(1,nr_letters + 1):
+    password += random.choice(letters)
+for car in range(1,nr_symbols + 1):
+    password += random.choice(symbols)
+for car in range(1,nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(password)
